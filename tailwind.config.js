9a2129977ba1c3ee363f8d47c8d89e281de4f2cf
Anwyll Le <lelinhnguyen2109@gmail.com>
2023-08-31 18:04:16 -0400

    added theme color

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,16 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        primary: '#3A506b',
+        secondary: '#5BC0BE',
+        tertiary: '#6FFFE9',
+        quaternary: '#1C2541',
+        quinary: '#0B132B',
+      }
+    },
   },
   plugins: [],
 }
+
