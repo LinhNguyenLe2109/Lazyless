723eac9a783177d4593a8a495621499c0589f6a0
Anwyll Le <lelinhnguyen2109@gmail.com>
2023-09-01 07:32:33 -0400

    added home route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component'
 
 const routes: Routes = [
-  {path: '',
-  component: HomeComponent,
-  title: 'Home page'},
+  {
+    path: '',
+    component: HomeComponent,
+    title: 'Home page'
+  },
 ];
 
 @NgModule({
