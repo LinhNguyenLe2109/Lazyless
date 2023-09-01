061a37899db4ba40be40b2058d4c65543b62b1ed
Anwyll Le <lelinhnguyen2109@gmail.com>
2023-09-01 08:31:02 -0400

    wokring on main page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,9 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   template: `
-    <p>
-      home works!
-    </p>
+    <div class="h-screen w-full flex justify-center items-center">
+      <div id="card" class="bg-secondary border-4 border-primary rounded-md px-8 py-5 text-center">
+        <h1 class="" id="appTitle">Lazyless</h1>
+        <p>A tool to help you improve your work efficiency for a specific duration, from one day to up to a month.</p>
+        <button>Let's start</button>
+      </div>
+
+    </div>
   `,
   styleUrls: ['./home.component.css']
 })
