5782ab61ccebb90951be42bf3ef67966c4ca2700
Anwyll Le <lelinhnguyen2109@gmail.com>
2023-09-03 06:13:38 -0400

    finished the front page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,9 +4,9 @@ import { Component } from '@angular/core';
   selector: 'app-home',
   template: `
     <div class="h-screen w-full flex justify-center items-center">
-      <div id="card" class="bg-secondary border-4 border-primary rounded-md px-8 py-5 text-center">
+      <div id="card" class="bg-secondary border-4 border-primary rounded-md px-10 py-5 text-center">
         <h1 id="appTitle">Lazyless</h1>
-        <p class="text-lg">A tool to help you improve your work efficiency for a specific duration, from one day to up to a month.</p>
+        <p class="text-lg mb-8">A tool to help you improve your work efficiency for a specific duration, from one day to up to a month.</p>
         <button class="bg-quinary text-majorText px-6 py-3 rounded-lg">Let's start</button>
       </div>
 
