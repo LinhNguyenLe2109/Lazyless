c6d1cf4bd68dd8faf745453c5e8c5ec6bf4eb523
Anwyll Le <lelinhnguyen2109@gmail.com>
2023-09-05 06:09:44 -0400

    switched to section

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,14 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   template: `
-    <div class="h-screen w-full flex justify-center items-center">
+    <section class="h-screen w-full flex justify-center items-center">
       <div id="card" class="bg-secondary border-4 border-primary rounded-md px-10 py-5 text-center">
         <h1 id="appTitle">Lazyless</h1>
         <p class="text-lg mb-8">A tool to help you improve your work efficiency for a specific duration, from one day to up to a month.</p>
         <button routerLink="/dailyTask" class="bg-quinary text-majorText px-6 py-3 rounded-lg">Let's start</button>
       </div>
 
-    </div>
+    </section>
   `,
   styleUrls: ['./home.component.css']
 })
