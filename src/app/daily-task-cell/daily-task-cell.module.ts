7d8023fee5db11fb430cd3e4fa87dadf0267ee8a
Anwyll Le <lelinhnguyen2109@gmail.com>
2023-09-06 22:36:12 -0400

    working on cell data

--- src/app/daily-task-cell/daily-task-cell.module.ts
+++ src/app/daily-task-cell/daily-task-cell.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ContentCellComponent } from './content-cell/content-cell.component';
+import { TaskContentComponent } from './task-content/task-content.component';
+import { TaskInputComponent } from './task-input/task-input.component';
 
 
 @NgModule({
-  declarations: [ContentCellComponent],
+  declarations: [ContentCellComponent, TaskContentComponent, TaskInputComponent],
   imports: [
     CommonModule
   ],
---
+++ src/app/daily-task-cell/task-content/task-content.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task-content',
+  templateUrl: './task-content.component.html',
+  styleUrls: ['./task-content.component.css']
+})
+export class TaskContentComponent {
+
+}
