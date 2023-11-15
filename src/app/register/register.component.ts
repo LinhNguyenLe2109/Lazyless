8c66b2fe8267ab443ebed581fd6f600cb72e9033
Anwyll Le <lelinhnguyen2109@gmail.com>
2023-11-15 01:30:11 -0500

    added register page

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent {
+  registerForm = new FormGroup({
+    username: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required]),
+    confirmPassword: new FormControl('', [Validators.required]),
+  });
 
+  submitButtonClicked = false;
+
+  onSubmit() {
+    this.submitButtonClicked = true;
+    this.checkMatchingPasswords();
+    if (this.registerForm.invalid) {
+    } else {
+    }
+  }
+
+  checkMatchingPasswords() {
+    const password = this.registerForm.get('password')?.value;
+    const confirmPassword = this.registerForm.get('confirmPassword')?.value;
+    if (password !== confirmPassword) {
+      this.registerForm.get('confirmPassword')?.setErrors({ mismatch: true });
+    } else {
+      this.registerForm.get('confirmPassword')?.setErrors(null);
+    }
+  }
 }
