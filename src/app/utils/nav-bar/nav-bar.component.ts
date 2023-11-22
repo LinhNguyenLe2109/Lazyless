ba7a5261b7a8cf081620187e9b3d00bbbbe6ed19
Anwyll Le <lelinhnguyen2109@gmail.com>
2023-11-22 15:20:28 -0500

    updated nav bar

--- src/app/utils/nav-bar/nav-bar.component.ts
+++ src/app/utils/nav-bar/nav-bar.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
-  styleUrls: ['./nav-bar.component.css']
+  styleUrls: ['./nav-bar.component.css'],
 })
 export class NavBarComponent {
+  constructor(public authService: AuthService) {}
 
+  logout() {
+    this.authService.logout();
+  }
 }
