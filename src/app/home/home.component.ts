4f0daab3c102666eafd7a083a2e78ff595d0037b
Anwyll Le <lelinhnguyen2109@gmail.com>
2024-01-02 16:47:11 -0500

    simplified home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,21 +3,29 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   template: `
-    <section class="h-screen w-full flex justify-center items-center">
-      <div
-        id="card"
-        class="bg-secondary border-4 border-primary rounded-md text-center w-11/12 md:w-9/12 p-2 "
+    <section class="min-h-screen w-full flex flex-col justify-center items-center">
+      <h1
+        id="appTitle"
+        class="!font-heading !text-5xl sm:!text-6xl md:!text-7xl xl:!text-8xl !font-semibold leading-none !mb-8 text-third"
       >
-        <h1 id="appTitle">Lazyless</h1>
-        <p class="sm:text-lg mb-8">
-          A tool to help you improve your work efficiency for a specific
-          duration, from one day to up to a month.
-        </p>
+        Introducing Lazyless
+      </h1>
+      <p class="text-2xl text-gray-400 mb-8">
+        A tool to help you improve your work efficiency for a specific duration,
+        from one day to up to a month.
+      </p>
+      <div class="my-4">
         <button
+          class="group inline-flex w-full md:w-auto h-14 mb-4 md:mr-3 px-7 items-center justify-center text-base font-medium text-third hover:text-white bg-fifth hover:bg-primary  transition duration-200 rounded-full"
           routerLink="/dailyTable"
-          class="bg-fifth text-third px-6 py-3 rounded-lg mb-3"
         >
-          Let's start
+          Start with daily table
+        </button>
+        <button
+          class="group inline-flex w-full md:w-auto h-14 mb-4 md:mr-3 px-7 items-center justify-center text-base font-medium text-third hover:text-white bg-fifth hover:bg-primary transition duration-200 rounded-full"
+          routerLink="/dailyLog"
+        >
+          Start with daily log
         </button>
       </div>
     </section>
